b9956acdc1378401b0c0e2072809bfb7ee96d5bd
Baumzin <brenosuvale@gmail.com>
2025-09-14 12:34:27 -0300

    Footer concluído com sucesso

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,23 @@ import Image from "next/image"
 
 export default function Footer() {
     return (
-        <footer className="footer">    
-            <Image src="/iconeFooter.png" alt="icone Oris" width={80} height={22}></Image>
-            <p className="Description">App de hábitos que entende sua rotina e se adapta a ela. integrado com IA e o calendário pessoal, Oris sabe os momentos livres e te manda um lembrete no horário certo.</p>
-            <p>© 2025 Oris. Todos os direitos reservados.</p>
+        <footer className="footer">
+            <div className="leftSide">
+                <Image src="/iconeFooter.png" alt="icone Oris" width={80} height={22} />
+                <p className="Description">App de hábitos que entende sua rotina e se adapta a ela. integrado com IA e o calendário pessoal, Oris sabe os momentos livres e te manda um lembrete no horário certo.</p>
+                <p>© 2025 Oris. Todos os direitos reservados.</p>
+            </div>
+
+            <div className="rightSide">
+                <div className="Up">
+                    <p>Termos de Uso</p>
+                    <p>Política de Privacidade</p>
+                </div>
+                <div className="Down">
+                    <p>Política de Cancelamento</p>
+                    <p>Política de reembolso</p>
+                </div>
+            </div>
         </footer>
     )
 }
