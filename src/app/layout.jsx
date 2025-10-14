4d86ba69e27c56df5b6b79c709c45cd9405cb3db
Breno <brenosuvale@gmail.com>
2025-10-14 08:57:55 -0300

    Adicionando square novamente

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "@/components/MainContainer/MainContainer.module.scss";
-import "@/app/styles/globals.scss";
-import "@/app/styles/responsive.scss";
+import "@/app/styles/globals.scss"; 
 import Footer from "@/components/Footer/Footer";
 import NavBar from "@/components/NavBar/Navbar";
 import LenisProvider from "@/components/Scroll/lenisProvider";
